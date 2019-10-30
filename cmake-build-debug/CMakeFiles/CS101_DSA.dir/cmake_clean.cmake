cc87d22edcf27dc4e96f1a47fe463d41823edab1
Boris_Yang <yangbo3@shanghaitech.edu.cn>
2019-10-30 20:08:58 +0800

    final-hw2-clion

---
+++ cmake-build-debug/CMakeFiles/CS101_DSA.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS101_DSA.dir/hw2/hw2-1.cpp.o"
+  "CS101_DSA"
+  "CS101_DSA.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS101_DSA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
