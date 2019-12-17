76814b947ece6b72c2c82e3472d5bbd8f9b47e07
Boris_Yang <yangbo3@shanghaitech.edu.cn>
2019-12-17 08:37:03 +0800

    hw4-3 ac

---
+++ hw4/hw4-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#include <string>
+int main(){
+    int N=0;
+    int M=0;
+    cin>>N;
+    string str_1;
+    cin>>str_1;
+    cin>>M;
+    string str_2;
+    cin>>str_2;
+    int match_table[5][5]={{5,-1,-2,-1,-3},{-1,5,-3,-2,-4},{-2,-3,5,-2,-2},{-1,-2,-2,5,-1},{-3,-4,-2,-1,-999999}};
+//    for (int i = 0; i < 5; ++i) {
+//        for (int j = 0; j < 5; ++j) {
+//            cout<<match_table[i][j]<<" ";
+//        }
+//        cout<<endl;
+//    }
+    
+
+}
--- hw4/hw4-3.cpp
+++ hw4/hw4-3.cpp
@@ -1,2 +1,48 @@
 #include <iostream>
 using namespace std;
+void quickSort(int s[], int l, int r) {
+    if (l < r) {
+        int i = l, j = r, x = s[l];
+        while (i < j) {
+            while (i < j && s[j] >= x)
+                j--;
+            if (i < j)
+                s[i++] = s[j];
+            while (i < j && s[i] < x)
+                i++;
+            if (i < j)
+                s[j--] = s[i];
+        }
+        s[i] = x;
+        quickSort(s, l, i - 1);
+        quickSort(s, i + 1, r);
+    }
+}
+int main(){
+    int N,M;
+    cin>>N>>M;
+    int* dragon_hp= new int[N];
+    int* knight_price =new int[M];
+    for (int i = 0; i <N ; ++i) {
+        cin>>dragon_hp[i];
+    }
+    for (int j = 0; j <M ; ++j) {
+        cin>>knight_price[j];
+    }
+    quickSort(dragon_hp,0,N-1);
+    quickSort(knight_price,0,M-1);
+    int p=0;
+    int q=0;
+    int cnt=0;
+    while(p<N && q<M){
+        if(dragon_hp[p]<=knight_price[q]){
+            cnt+=knight_price[q];
+            q++;
+            p++;
+        } else{q++;}
+    }
+    if(p==N){cout<<cnt;}
+    else{cout<<"you died!";}
+
+    return 0;
+}
